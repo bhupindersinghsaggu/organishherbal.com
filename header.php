e9d33377dbf29008458228ca09bb2dd9ed5829a8
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-22 13:39:05 +0530

    add header file code

---
+++ header.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from organishherbal.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Jul 2024 07:02:37 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <!-- metas -->
+    <meta charset="utf-8">
+    <meta name="author" content="Organish Herbal" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="keywords" content="Spirulina | Moringa" />
+    <meta name="description" content="Herbal Products" />
+
+    <!-- title  -->
+    <title>Organish Herbal-Herbal Products | Spirulina | Moringa
+
+    </title>
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="theme/img/favicon.ico" />
+
+
+    <!-- plugins -->
+    <link rel="stylesheet" href="theme/css/plugins.css">
+
+    <!-- search css -->
+    <link rel="stylesheet" href="theme/search/search.css">
+
+    <!-- quform css -->
+    <link rel="stylesheet" href="theme/quform/css/base.css">
+
+    <!-- theme core css -->
+    <link href="theme/css/styles.css" rel="stylesheet">
+
+    <!-- theme custom css -->
+    <link href="theme/css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header>
+        <div class="main-wrapper">
+            <!-- HEADER -->
+            <div class="top-bar bg-secondary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-9 col-xs-12">
+                            <div class="top-bar-info">
+                                <ul class="ps-0">
+                                    <li><a href="tel:8008280032"><i class="ti-mobile"></i>8008280031</a></li>
+                                    <li class="d-none d-sm-inline-block"><i class="ti-email"></i><a
+                                            href="mailto:info@organishherbal.com">info@organishherbal.com</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-3 d-none d-md-block">
+                            <ul class="top-social-icon ps-0">
+                                <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-default">
+                <!-- start top search -->
+                <div class="top-search bg-primary">
+                    <div class="container">
+                        <form class="search-form" action="#" method="GET" accept-charset="utf-8">
+                            <div class="input-group">
+                                <span class="input-group-addon cursor-pointer">
+                                    <button class="search-form_submit fas fa-search text-white" type="submit"></button>
+                                </span>
+                                <input type="text" class="search-form_input form-control" name="s"
+                                    autocomplete="off" placeholder="Type & hit enter...">
+                                <span class="input-group-addon close-search mt-1"><i class="fas fa-times"></i></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- end top search -->
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-lg-12">
+                            <div class="menu_area alt-font">
+                                <nav class="navbar navbar-expand-lg navbar-light p-0">
+                                    <div class="navbar-header navbar-header-custom">
+                                        <!-- start logo -->
+                                        <a href="index.html" class="navbar-brand logodefault"><img id="logo"
+                                                src="theme/img/logos/logo.png" alt="logo"></a>
+                                        <!-- end logo -->
+                                    </div>
+                                    <div class="navbar-toggler"></div>
+                                    <!-- start menu area -->
+                                    <ul class="navbar-nav ms-auto" id="nav" style="display: none;">
+                                        <li><a href="index.html">Home</a>
+                                        </li>
+                                        <li><a href="#">Products</a>
+                                            <ul>
+                                                <li><a href="spirulina.html">Health Care</a>
+                                                    <ul>
+                                                        <li><a href="spirulina.html">Spirulina</a></li>
+                                                        <li><a href="moringa.html">Moringa</a>
+                                                        <li><a href="triphala.html">Triphala</a>
+                                                        <li><a href="six-m-syrup.html">Six M Syrup</a>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="spirulina.html">Pet Care</a>
+                                                    <ul>
+                                                        <li><a href="all-in-one.html">All in One</a>
+                                                        <li><a href="power-vita-gold.html">Power Vita Gold</a>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="spices.html">Spices</a></li>
+                                                <li><a href="new-arrivals.html">New Arrivals</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="about.html">About us</a>
+                                        </li>
+                                        <li><a href="contact-us.html">Contact us</a>
+                                        </li>
+                                    </ul>
+                                    <!-- end menu area -->
+                                    <!-- start attribute navigation -->
+                                    
+                                    <!-- end attribute navigation -->
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
