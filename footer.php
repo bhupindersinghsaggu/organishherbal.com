23f16f41f5acb1455118d8fe9a2fbc8a54066ba0
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-07-24 16:41:00 +0530

    address and contact no change

--- footer.php
+++ footer.php
@@ -1,132 +1,133 @@
 <footer>
+    <div class="container">
+        <div class="row mt-n2-6">
+            <div class="col-sm-6 col-lg-3 mt-2-6">
+                <div class="footer-logo mb-1-9">
+                    <h3 class="title-font text-secondary mb-4 h4">About us</h3>
+                </div>
+                <p class="text-white mb-1-6 mb-lg-1-9 opacity9">ORGANISH HERBAL
+                    is leading manufacturer and supplier of ayurvedic and herbal products. At our core, we are
+                    committed to providing you with the purest and highest-quality herbal products. We source our
+                    ingredients from sustainable and responsible suppliers, ensuring that each product is crafted
+                    with care and respect for the environment.</p>
+            </div>
+            <div class="col-sm-6 col-lg-3 mt-2-6">
+                <div class="ps-sm-1-6 ps-md-1-9 ps-xl-2-9">
+                    <h3 class="title-font text-secondary mb-4 h4">Quick Links</h3>
+                    <ul class="footer-list-style1">
+
+                        <li><a href="spirulina.php">Spirulina</a></li>
+                        <li><a href="moringa.php">Moringa</a></li>
+                        <li><a href="all-in-one.php">All in One</a></li>
+                        <li><a href="power-vita-gold.php">Power Vita Gold</a></li>
+                        <li><a href="spices.php">Spices</a></li>
+                        <li><a href="new-arrivals.php">New Arrivals</a></li>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contact-us.php">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 mt-2-6">
+                <div class="ps-sm-1-9">
+                    <h3 class="title-font text-secondary mb-4 h4">Locate us</h3>
+                    <ul class="footer-hour-list">
+                        <li>
+                            <i class="ti-time text-white"></i>
+                            <div>
+                                <h4 class="h6 text-white">Locate us</h4>
+                                <p class="mb-0 text-white display-30">
+                                    SCO No. 211, 2nd Floor, Town Center TDI City , Sec - 36, Panipat , Haryana - 132103
+                                    , INDIA</p>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="ti-time text-white"></i>
+                            <div>
+                                <h4 class="h6 text-white">Contact us</h4>
+                               <a href="mailto:info@organishherbal.com"> <p class="mb-0 text-white display-30">info@organishherbal.com</p></a>
+                            </div>
+                        </li>
+                        <li>
+                            <i class="ti-time text-white"></i>
+                            <div>
+                                <h4 class="h6 text-white">Call us</h4>
+                               <a href="tel:+918008280031"> <p class="mb-0 text-white display-30">8008280031</p></a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 mt-2-6" id="contact-us">
+                <div class="ps-sm-1-9">
+                    <h3 class="title-font text-secondary mb-4 h4">Reach us</h3>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3474.8830281717915!2d76.95435931510083!3d29.432214982109105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjnCsDI1JzU2LjAiTiA3NsKwNTcnMjMuNiJF!5e0!3m2!1sen!2sin!4v1690857844099!5m2!1sen!2sin"
+                        width="180" height="180" style="border:0;" allowfullscreen="" loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bar">
         <div class="container">
-            <div class="row mt-n2-6">
-                <div class="col-sm-6 col-lg-3 mt-2-6">
-                    <div class="footer-logo mb-1-9">
-                        <h3 class="title-font text-secondary mb-4 h4">About us</h3>
-                    </div>
-                    <p class="text-white mb-1-6 mb-lg-1-9 opacity9">ORGANISH HERBAL
-                        is leading manufacturer and supplier of ayurvedic and herbal products. At our core, we are
-                        committed to providing you with the purest and highest-quality herbal products. We source our
-                        ingredients from sustainable and responsible suppliers, ensuring that each product is crafted
-                        with care and respect for the environment.</p>
+            <div class="row align-items-center">
+                <div class="col-md-6 text-center text-md-start mt-3 mt-md-0 order-2 order-md-1">
+                    <p class="d-inline-block text-white mb-0 display-30 display-lg-29">&copy; <span
+                            class="current-year"></span> ORGANISH HERBAL | Website by <a href="https://boldally.com/"
+                            target="_blank" class="text-secondary">Boldally</a>
+                    </p>
                 </div>
-                <div class="col-sm-6 col-lg-3 mt-2-6">
-                    <div class="ps-sm-1-6 ps-md-1-9 ps-xl-2-9">
-                        <h3 class="title-font text-secondary mb-4 h4">Quick Links</h3>
-                        <ul class="footer-list-style1">
+                <div class="col-md-6 text-md-end order-1 order-md-2">
+                    <p class="text-white d-inline-block font-weight-600 mb-0 align-middle me-3">Follw Us :</p>
+                    <ul class="footer-social-style1">
+                        <li>
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                        </li>
 
-                            <li><a href="spirulina.php">Spirulina</a></li>
-                            <li><a href="moringa.php">Moringa</a></li>
-                            <li><a href="all-in-one.php">All in One</a></li>
-                            <li><a href="power-vita-gold.php">Power Vita Gold</a></li>
-                            <li><a href="spices.php">Spices</a></li>
-                            <li><a href="new-arrivals.php">New Arrivals</a></li>
-                            <li><a href="about.php">About Us</a></li>
-                            <li><a href="contact-us.php">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-3 mt-2-6">
-                    <div class="ps-sm-1-9">
-                        <h3 class="title-font text-secondary mb-4 h4">Locate us</h3>
-                        <ul class="footer-hour-list">
-                            <li>
-                                <i class="ti-time text-white"></i>
-                                <div>
-                                    <h4 class="h6 text-white">Locate us</h4>
-                                    <p class="mb-0 text-white display-30">
-                                        SCO No. 211, 2nd Floor, Town, Center TDI City,
-                                        Sec.-36, Panipat-132103 (Haryana)</p>
-                                </div>
-                            </li>
-                            <li>
-                                <i class="ti-time text-white"></i>
-                                <div>
-                                    <h4 class="h6 text-white">Contact us</h4>
-                                    <p class="mb-0 text-white display-30">info@organishherbal.com</p>
-                                </div>
-                            </li>
-                            <li>
-                                <i class="ti-time text-white"></i>
-                                <div>
-                                    <h4 class="h6 text-white">Call us</h4>
-                                    <p class="mb-0 text-white display-30">8008280031</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-3 mt-2-6" id="contact-us">
-                    <div class="ps-sm-1-9">
-                        <h3 class="title-font text-secondary mb-4 h4">Reach us</h3>
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3474.8830281717915!2d76.95435931510083!3d29.432214982109105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjnCsDI1JzU2LjAiTiA3NsKwNTcnMjMuNiJF!5e0!3m2!1sen!2sin!4v1690857844099!5m2!1sen!2sin"
-                            width="180" height="180" style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="footer-bar">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-md-6 text-center text-md-start mt-3 mt-md-0 order-2 order-md-1">
-                        <p class="d-inline-block text-white mb-0 display-30 display-lg-29">&copy; <span
-                                class="current-year"></span> ORGANISH HERBAL | Website by <a
-                                href="https://boldally.com/" target="_blank" class="text-secondary">Boldally</a>
-                        </p>
-                    </div>
-                    <div class="col-md-6 text-md-end order-1 order-md-2">
-                        <p class="text-white d-inline-block font-weight-600 mb-0 align-middle me-3">Follw Us :</p>
-                        <ul class="footer-social-style1">
-                            <li>
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    </div>
+</footer>
 
 
-    <a href="#!" class="scroll-to-top"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
+<a href="#!" class="scroll-to-top"><i class="fas fa-angle-up" aria-hidden="true"></i></a>
 
-    <!-- all js include start -->
+<!-- all js include start -->
 
-    <!-- jQuery -->
-    <script src="theme/js/jquery.min.js"></script>
+<!-- jQuery -->
+<script src="theme/js/jquery.min.js"></script>
 
-    <!-- popper js -->
-    <script src="theme/js/popper.min.js"></script>
+<!-- popper js -->
+<script src="theme/js/popper.min.js"></script>
 
-    <!-- bootstrap -->
-    <script src="theme/js/bootstrap.min.js"></script>
+<!-- bootstrap -->
+<script src="theme/js/bootstrap.min.js"></script>
 
-    <!-- jquery -->
-    <script src="theme/js/core.min.js"></script>
+<!-- jquery -->
+<script src="theme/js/core.min.js"></script>
 
-    <!-- search -->
-    
+<!-- search -->
 
-    <!-- theme core scripts -->
-    <script src="theme/js/main.js"></script>
 
-    <!-- form plugins js -->
-    <script src="theme/quform/js/plugins.js"></script>
+<!-- theme core scripts -->
+<script src="theme/js/main.js"></script>
 
-    <!-- form scripts js -->
-    <script src="theme/quform/js/scripts.js"></script>
+<!-- form plugins js -->
+<script src="theme/quform/js/plugins.js"></script>
 
-    <!-- all js include end -->
+<!-- form scripts js -->
+<script src="theme/quform/js/scripts.js"></script>
+
+<!-- all js include end -->
 
 </body>
 
 
 <!-- Mirrored from organishherbal.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Jul 2024 07:02:49 GMT -->
+
 </html>
