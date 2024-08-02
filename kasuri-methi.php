dc2021c1f699c4c9b95c3baa79903ab61aed6e9d
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-08-02 13:58:15 +0530

    KASURI METHI

---
+++ kasuri-methi.php
@@ -0,0 +1,142 @@
+<?php
+
+include('header.php');
+include('function.php');
+
+?>
+<section class="page-title-section bg-img cover-background" data-overlay-dark="6" data-background="/theme/img/bg/sabji-masala-bg.jpg" style="background-image: url(_/theme/img/bg/sabji-masala-bg.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+</section>
+<section id="moringa">
+    <div class="container">
+        <!-- <div class="product-heading">
+            <h4 class="about-box2 border-radius-10">
+                Discover the magic of spices and transform every meal
+            </h4>
+        </div> -->
+        <div class="row align-items-center ">
+            <div class="col-lg-6 mb-1-9 mb-lg-0">
+                <div class="position-relative pe-lg-1-9 pe-xl-7 ps-xl-8 text-center about-style01">
+                    <img src="theme/img/product/sabji-masala.jpg" class="rounded z-index-1 position-relative" alt="...">
+                    <h5 class="mt-3">AVAILABILITY: 50 g | 100g </h5>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="">
+                    <h4 class="display-20 display-sm-15 display-xl-9 mb-3" style="font-size: 35px;">Sabji Masala
+                    </h4>
+                    <p>Kasuri Methi refers to dried leaves of the fenugreek plant. It is an herb with a bitter but addictive taste. Used as a spice in Indian curries, sabzi, parathas its strong aroma and distinctive flavour fascinate the taste buds!These dried, fragrant leaves are light-green in colour, and nutty, savoury, and slightly bitter in taste. Its aroma is pungent and strong on the nose, however, when added to dishes, its flavour disperses and blends through seamlessly and mellowly. 
+                    </p>
+                    <div class="col-sm-12">
+                        <h5>-About this item</h5>
+                        <ul class="list-style2 mb-0">
+                            <li> Make your dishes tempting and irresistible</li>
+                            <li> Experience the freshness of flavour and aroma of this dish that is packaged using flavour lock technology and contains no preservatives</li>
+                            <li> Made from high-quality ingredients, contains no added artificial – colours and flavours and is sealed with care</li>
+                        </ul><br>
+                        <a href=<?php echo " $shoplink " ?> target=_blank class="butn secondary">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 mb-2-9">
+                <div class="row padding-top50">
+                    <div class="col-lg-6 mb-1-9 mb-lg-0">
+                        <div class="card border-0 border-radius-none">
+                            <div class="card-body p-0">
+                                <div class="border-bottom border-width-2 pb-3 mb-4">
+                                </div>
+                                <ul class="list-style5 text-center text-sm-start">
+                                    <li>
+                                        <div class="text-center text-sm-start d-sm-flex mb-4">
+                                            <div class="">
+                                                <h4 class="h5">Health Benefits</h4>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="nutritional-info">
+                                                <ul class="list-style1 mb-0">
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>
+                                                        Reduces cholesterol </h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>Improve Digestion</h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>For weight loss </h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>Control diabetes</h4>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mb-1-9 mb-lg-0">
+                        <div class="card border-0 border-radius-none">
+                            <div class="card-body p-0">
+                                <div class="border-bottom border-width-2 pb-3 mb-4">
+                                </div>
+                                <ul class="list-style5 text-center text-sm-start">
+                                    <li>
+                                        <div class="text-center text-sm-start d-sm-flex mb-4">
+                                            <div class="flex-shrink-0 mb-4 mb-md-0">
+                                                <img src="theme/img/product/sabji-masala-round.jpg" class="rounded-circle" alt="...">
+                                            </div>
+                                            <div class="flex-grow-1 ms-sm-4">
+                                                <h4 class="h5">Ingerdients</h4>
+                                                <p class="mb-0 ingerdients-info">Nutrition Value Per 100g</p>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Energy (Calculated) </h4>
+                                                <span class="seprator"></span>
+                                                <span class="text-muted font-weight-600">323 kcal</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Total Carbohydrates</h4>
+                                                <span class="seprator"></span>
+                                                <span>58 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Protein</h4>
+                                                <span class="seprator"></span>
+                                                <span>23 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Total Fat</h4>
+                                                <span class="seprator"></span>
+                                                <span>6.5 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Cholesterol</h4>
+                                                <span class="seprator"></span>
+                                                <span>0 g</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+
+include('latest-product.php');
+include('footer.php');
+
+?>
