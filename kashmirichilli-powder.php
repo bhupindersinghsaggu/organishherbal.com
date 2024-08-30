b49d305b5fda257b38a1c5aa4ffbb4e0d93aa042
Bhupinder Singh Saggu <55052881+bhupindersinghsaggu@users.noreply.github.com>
2024-08-30 15:38:30 +0530

    Update kashmirichilli-powder.php

--- kashmirichilli-powder.php
+++ kashmirichilli-powder.php
@@ -1,13 +1,10 @@
 <?php
 
 include('header.php');
-
-include('function.php');
+include('function.php')
 
 ?>
-<section class="page-title-section bg-img cover-background" data-overlay-dark="6"
-    data-background="/theme/img/bg/coriander-powder-bg.jpg"
-    style="background-image: url(_/theme/img/bg/coriander-powder-bg.jpg);">
+<section class="page-title-section bg-img cover-background" data-overlay-dark="6" data-background="/theme/img/bg/kashmirichilli-powder-bg.jpg" style="background-image: url(_/theme/img/bg/kashmirichilli-powder-bg.jpg);">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -15,165 +12,152 @@ include('function.php');
         </div>
     </div>
 </section>
-<div class="sidebar ttm-sidebar-right ttm-bgcolor-white clearfix">
+<section id="moringa">
     <div class="container">
-        <!-- row -->
-        <div class="row">
-            <div class="col-lg-12 content-area">
-                <div class="ttm-single-product-details product">
-                    <div class="ttm-single-product-info clearfix">
-                        <div class="product-gallery images product-center">
-                            <figure class="ttm-product-gallery__wrapper">
-                                <div class="product-gallery__image">
-                                    <img class="img-fluid" src="images/product/kashmirichilli-powder.jpg"
-                                        alt="product-img">
-                                    <h6 class="mt-3 txt-center size-heading">AVAILABILITY: 50 g | 100g | </h6>
+        <!-- <div class="product-heading">
+            <h4 class="about-box2 border-radius-10">
+                Discover the magic of spices and transform every meal
+            </h4>
+        </div> -->
+        <div class="row align-items-center ">
+            <div class="col-lg-6 mb-1-9 mb-lg-0">
+                <div class="position-relative pe-lg-1-9 pe-xl-7 ps-xl-8 text-center about-style01">
+                    <img src="theme/img/product/kashmirichilli-powder.jpg" class="rounded z-index-1 position-relative" alt="...">
+                    <h5 class="mt-3">AVAILABILITY: 100 g | 200g | 500 g</h5>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="">
+                    <h4 class="display-20 display-sm-15 display-xl-9 mb-3" style="font-size: 35px;">Kashmiri Chilli
+                        Powder
+                    </h4>
+                    <p>Vibrant color - our Kashmiri chili has a distinctive red hue which makes it great for grilling
+                        meats and dying curries a vibrant red color. Complex citrusy and slightly smoky flavor - This
+                        chili isn't just one note but is so complex in its flavor which adds depth to dishes. The chili
+                        powder will add a delicious and spicy flavor to your dish, as well as helping to tenderize the
+                        meat. Soups and Stews: Kashmiri chili powder can be used to add depth and complexity to soups
+                        and stews. Simply add a teaspoon or two of the powder to the pot when you add your other
+                        ingredients.
+                    </p>
+                    <div class="col-sm-12">
+                        <h5>-About this item</h5>
+                        <ul class="list-style2 mb-0">
+                            <li>100% natural:- Organish Herbal products are 100% natural and best suitable for your
+                                health. </li>
+                            <li>Versatile Use: This spice can be used in a wide variety of dishes, including curries, marinades, sauces, grills and tandoori preparations, adding depth of flavour </li>
+                            <li>Distinctive flavour: Experience the culinary magic of Kashmiri Red Chilli Powder, where the exceptional taste of Kashmiri chillies will flavor up your taste buds. </li>
+                            <li>Quality:- All the Indian spices used in this product are of premium quality</li>
+                        </ul><br>
+                        <a href=<?php echo " $shoplink " ?> target=_blank class="butn secondary">Buy Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 mb-2-9">
+                <div class="row padding-top50">
+                    <div class="col-lg-6 mb-1-9 mb-lg-0">
+                        <div class="card border-0 border-radius-none">
+                            <div class="card-body p-0">
+                                <div class="border-bottom border-width-2 pb-3 mb-4">
                                 </div>
-                            </figure>
-
-                        </div>
-                        <div class="summary entry-summary">
-                            <p class="price mb-25">
-                                <span class="Price-amount amount">
-                                    <h2>Kashmiri Chilli Powder
-                                    </h2>
-                                </span>
-                            </p>
-                            <div class="product-details__short-description">
-                                <p>our Kashmiri chilli has a distinctive red hue which makes it great for grilling meats
-                                    and dying curries a vibrant red color. Complex citrusy and slightly smoky flavor -
-                                    This chilli isn't just one note but is so complex in its flavor which adds depth to
-                                    dishes. The chili powder will add a delicious and spicy flavor to your dish, as well
-                                    as helping to tenderize the meat. Soups and Stews: Kashmiri chili powder can be used
-                                    to add depth and complexity to soups and stews. Simply add a teaspoon or two of the
-                                    powder to the pot when you add your other ingredients.</p>
-                            </div>
-                            <div class="content-inner active">
-                                <h3>About this Item</h3>
-                                <ul class="l-height">
-                                    <li>100% natural:- Organish Herbal products are 100% natural and best suitable for
-                                        your
-                                        health. </li>
-                                    <li>Uses For:- Turmeric powder is used in all Indian cuisines and it's perfect for
-                                        vegetables, meats, gravies and curries. </li>
-                                    <li>Ready To Use:- Turmeric powder brings out a freshness with its aroma, colour and
-                                        taste
-                                        to any dish. </li>
-                                    <li>Quality:- All the Indian spices used in this product are of premium quality</li>
+                                <ul class="list-style5 text-center text-sm-start">
+                                    <li>
+                                        <div class="text-center text-sm-start d-sm-flex mb-4">
+                                            <div class="">
+                                                <h4 class="h5">Health Benefits </h4>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="nutritional-info">
+                                                <ul class="list-style1 mb-0">
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>
+                                                            Rich in vitamin C </h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>Boosts
+                                                            immunity </h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>Protects
+                                                            your heart </h4>
+                                                    </li>
+                                                    <li>
+                                                        <h4 class="h6 mb-0"><i class="ti-check-box text-secondary me-3 font-weight-500"></i>Helps
+                                                            your digestive system </h4>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
                                 </ul>
                             </div>
-                            <form class="cart" action="#" method="post" enctype="multipart/form-data">
-                                <a href=<?php echo " $shoplink " ?> target=_blank
-                                    class="cart_button ttm-btn ttm-btn-bgcolor-black ttm-btn-shape-round ttm-textcolor-white">Buy
-                                    Now</a>
-                            </form>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mb-1-9 mb-lg-0">
+                        <div class="card border-0 border-radius-none">
+                            <div class="card-body p-0">
+                                <div class="border-bottom border-width-2 pb-3 mb-4">
+                                </div>
+                                <ul class="list-style5 text-center text-sm-start">
+                                    <li>
+                                        <div class="text-center text-sm-start d-sm-flex mb-4">
+                                            <div class="flex-shrink-0 mb-4 mb-md-0">
+                                                <img src="theme/img/product/kashmirichilli-round.jpg" class="rounded-circle" alt="...">
+                                            </div>
+                                            <div class="flex-grow-1 ms-sm-4">
+                                                <h4 class="h5">Ingerdients</h4>
+                                                <p class="mb-0 ingerdients-info">Nutrition Value Per 100g</p>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3">
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Energy (Calculated) </h4>
+                                                <span class="seprator"></span>
+                                                <span class="text-muted font-weight-600">284 kcal</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Total Carbohydrates</h4>
+                                                <span class="seprator"></span>
+                                                <span>54 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Protein</h4>
+                                                <span class="seprator"></span>
+                                                <span>6 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Total Fat</h4>
+                                                <span class="seprator"></span>
+                                                <span>7 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Saturated Fatty Acid</h4>
+                                                <span class="seprator"></span>
+                                                <span>3 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Total Trans Fatty Acid</h4>
+                                                <span class="seprator"></span>
+                                                <span>0 g</span>
+                                            </div>
+                                            <div class="nutritional-info">
+                                                <h4 class="h6 mb-0">Cholesterol</h4>
+                                                <span class="seprator"></span>
+                                                <span>0 g</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="additional-info">
-            <h4 class="font-25 pt-20 pd-20">Additional Information</h4>
-            <div class="row">
-                <div class="col-md-6">
-                    <div>
-                        <div class="tbl-padding-20">
-                            <table class="table table-striped">
-                                <tbody>
-                                    <tr
-                                        style="background-color: #00460f; padding: 10px;border-radius: 10px;color: #fff;">
-                                        <th scope="row" class="f-18">
-                                            Health Benefits </th>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"> <i
-                                                class="fa fa-angle-double-right ttm-textcolor-skincolor"></i>
-                                            Rich in vitamin C </th>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"> <i
-                                                class="fa fa-angle-double-right ttm-textcolor-skincolor"></i>
-                                            Boosts immunity </th>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"> <i
-                                                class="fa fa-angle-double-right ttm-textcolor-skincolor"></i>
-                                            Protects your heart
-                                        </th>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"> <i
-                                                class="fa fa-angle-double-right ttm-textcolor-skincolor"></i>
-                                            Helps your digestive system</th>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div><!-- content-inner end-->
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div>
-                        <div class="tbl-padding-20">
-                            <table class="table table-striped">
-                                <tbody>
-                                    <tr
-                                        style="background-color: #00460f; padding: 10px;border-radius: 10px;color: #fff;">
-                                        <th scope="row" class="f-18" colspan="2">Ingerdients (Nutrition Value Per 100g)
-                                        </th>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i> Energy (Calculated)</th>
-                                        <td>284 kcal
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i></i> Total Carbohydrates
-                                        </th>
-                                        <td>54 g</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i> Protein</th>
-                                        <td>6 g</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i> Total Fat</th>
-                                        <td>7 g</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i> Saturated Fatty Acid
-                                        </th>
-                                        <td>3 g</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor">
-                                            </i> Total Trans Fatty Acid
-                                        </th>
-                                        <td>0 g</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row"><i class="fa fa-angle-double-right ttm-textcolor-skincolor"></i>
-                                            Cholesterol
-                                        </th>
-                                        <td>0 g</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div><!-- content-inner end-->
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div><!-- row end -->
-</div>
+    </div>
+</section>
 <?php
 include('latest-product.php');
-
 include('footer.php');
 
-?>
+?> <a href=<?php echo " $shoplink " ?> target=_blank class="butn secondary">Buy Now</a>
